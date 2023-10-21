2067b45fa69f23509475a27352f82cd290bbc9b3
Hu Tianrun <tianrunhu@gmail.com>
2023-10-21 14:00:47 +0800

    building system

---
+++ urban-studio/core/segmentation.py
@@ -0,0 +1,13 @@
+from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
+from PIL import Image
+import requests
+
+processor = SegformerImageProcessor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
+model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits  # shape (batch_size, num_labels, height/4, width/4)
