a0c5e1aa1f2959ed506a00536670019660c0f7c1
Hu Tianrun <tianrunhu@gmail.com>
2023-12-11 13:35:49 +0800

    add utils

---
+++ src/utils/segmentation_utils.py
@@ -0,0 +1,56 @@
+def wait_continue(figure, timeout: float = 0, continue_key: str = ' ') -> int:
+    """Show the image and wait for the user's input.
+
+    This implementation refers to
+    https://github.com/matplotlib/matplotlib/blob/v3.5.x/lib/matplotlib/_blocking_input.py
+
+    Args:
+        timeout (float): If positive, continue after ``timeout`` seconds.
+            Defaults to 0.
+        continue_key (str): The key for users to continue. Defaults to
+            the space key.
+
+    Returns:
+        int: If zero, means time out or the user pressed ``continue_key``,
+            and if one, means the user closed the show figure.
+    """  # noqa: E501
+    import matplotlib.pyplot as plt
+    from matplotlib.backend_bases import CloseEvent
+    is_inline = 'inline' in plt.get_backend()
+    if is_inline:
+        # If use inline backend, interactive input and timeout is no use.
+        return 0
+
+    if figure.canvas.manager:  # type: ignore
+        # Ensure that the figure is shown
+        figure.show()  # type: ignore
+
+    while True:
+
+        # Connect the events to the handler function call.
+        event = None
+
+        def handler(ev):
+            # Set external event variable
+            nonlocal event
+            # Qt backend may fire two events at the same time,
+            # use a condition to avoid missing close event.
+            event = ev if not isinstance(event, CloseEvent) else event
+            figure.canvas.stop_event_loop()
+
+        cids = [
+            figure.canvas.mpl_connect(name, handler)  # type: ignore
+            for name in ('key_press_event', 'close_event')
+        ]
+
+        try:
+            figure.canvas.start_event_loop(timeout)  # type: ignore
+        finally:  # Run even on exception like ctrl-c.
+            # Disconnect the callbacks.
+            for cid in cids:
+                figure.canvas.mpl_disconnect(cid)  # type: ignore
+
+        if isinstance(event, CloseEvent):
+            return 1  # Quit for close.
+        elif event is None or event.key == continue_key:
+            return 0  # Quit for continue.
